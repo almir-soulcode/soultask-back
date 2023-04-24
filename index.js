c982c9f3dbeb89c3ad1b1877286eac32d10dbc29
jose-almir <jr.cod.dev@gmail.com>
2023-04-24 09:57:10 -0300

    Configuração inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+console.log(process.env.MONGODB_URL);
+
+// BREAK 10H05
+
+// 1) Conexão com Mongo usando URL
+// 2) Criar o modelo de Tarefa
+// 3) As rotas GET, POST, PUT e DELETE
